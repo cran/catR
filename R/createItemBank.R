6929a9731f1b400585849f4177890090bfd00022
David Magis <david.magis@ulg.ac.be>
2011-10-23 10:09:04 +0000

    version 2.0

--- R/createItemBank.R
+++ R/createItemBank.R
@@ -1,20 +1,34 @@
-createItemBank<-function(items=100,model="4PL",thMin=-4,thMax=4,step=0.01, seed=1, D=1){
-if (is.matrix(items)==TRUE) itemPar<-items
-else{
+createItemBank<-function(items=100, cb=FALSE, model="4PL", aPrior=c("norm",1,0.2),bPrior=c("norm",0,1),cPrior=c("unif",0,0.25),dPrior=c("unif",0.75,1),thMin=-4, thMax=4,step=0.01, seed=1, D=1){
+if (is.matrix(items) | is.data.frame(items)){
+itemPar<-as.matrix(items[,1:4])
+if (ncol(items)==5 & cb) cbGroup<-items[,5]
+else cbGroup<-NULL 
+}
+else {
+cbGroup<-NULL
+testB<-switch(bPrior[1],norm=1,unif=2)
+if (is.null(testB)) stop("Prior distribution for item difficulties must be either 'norm' or 'unif'",call.=FALSE)
+testA<-switch(aPrior[1],norm=1,lnorm=2,unif=3)
+if (is.null(testA)) stop("Prior distribution for item discriminations must be either 'norm', 'lnorm' or 'unif'",call.=FALSE)
+testC<-switch(cPrior[1],beta=1,unif=2)
+if (is.null(testC)) stop("Prior distribution for item lower asymptotes must be either 'beta' or 'unif'",call.=FALSE)
+testD<-switch(dPrior[1],beta=1,unif=2)
+if (is.null(testD)) stop("Prior distribution for item upper asymptotes must be either 'beta' or 'unif'",call.=FALSE)
 set.seed(seed)
-b<-rnorm(items)
-if (model!="1PL") a<-rnorm(items,1,0.2)
-else a<-rep(1,items)
-if (model=="3PL" | model=="4PL") c<-runif(items,0,0.25)
+b<-switch(bPrior[1],norm=rnorm(items,as.numeric(bPrior[2]),as.numeric(bPrior[3])),unif=runif(items,as.numeric(bPrior[1]),as.numeric(bPrior[2])))
+if (model!="1PL") a<-switch(aPrior[1],norm=rnorm(items,as.numeric(aPrior[2]),as.numeric(aPrior[3])),lnorm=rlnorm(items,as.numeric(aPrior[2]),as.numeric(aPrior[3])),unif=runif(items,as.numeric(aPrior[2]),as.numeric(aPrior[3])))
+else a<-rep(1,items) 
+if (model=="3PL" | model=="4PL") c<-switch(cPrior[1],beta=rbeta(items,as.numeric(cPrior[2]),as.numeric(cPrior[3])),unif=runif(items,as.numeric(cPrior[2]),as.numeric(cPrior[3])))
 else c<-rep(0,items)
-if (model=="4PL") d<-runif(items,0.75,1)
+if (model=="4PL") d<-switch(dPrior[1],beta=rbeta(items,as.numeric(dPrior[2]),as.numeric(dPrior[3])),unif=runif(items,as.numeric(dPrior[2]),as.numeric(dPrior[3])))
 else d<-rep(1,items)
 itemPar<-cbind(a,b,c,d)
 }
-colnames(itemPar)<-c("a","b","c","d")
-theta<-seq(from=thMin,to=thMax,by=step)
-infoTab<-matrix(NA,length(theta),nrow(itemPar))
+colnames(itemPar) <- c("a", "b", "c", "d")
+theta <- seq(from=thMin, to=thMax, by=step)
+infoTab <- matrix(NA, length(theta), nrow(itemPar))
 for (i in 1:length(theta)) infoTab[i,]<-Ii(theta[i],itemPar,D=D)$Ii
-res<-list(itemPar=itemPar,theta=theta,infoTab=infoTab)
-class(res)<-"itBank"
-return(res)}
+res <- list(itemPar=itemPar, theta=theta, infoTab=infoTab,cbGroup=cbGroup)
+class(res) <- "itBank"
+return(res)
+}
--- R/testList.R
+++ R/testList.R
@@ -1,104 +1,167 @@
-testList<-function(list,type="start"){
-argNames=ls()
-if (is.list(list)==FALSE) res<-list(test=FALSE,message=paste(deparse(substitute(list))," is not a list",sep=""))
-else{ 
-if (is.null(names(list))==TRUE) res<-list(test=FALSE,message=paste("list '",deparse(substitute(list)),"' has no argument names",sep=""))
-else{ 
-elements<-switch(type,
-start=c("fixItems","seed","nrItems","theta","halfRange","startSelect"),
-test=c("method","priorDist","priorPar","range","D","parInt","itemSelect","infoType"),
-stop=c("rule","thr","alpha"),
-final=c("method","priorDist","priorPar","range","D","alpha","parInt"))
-if (is.null(elements)==TRUE) res<-list(test=FALSE,message=paste("invalid 'type' argument ('",type,"' is not allowed)",sep=""))
-else{ 
-if (length(list)>length(elements)) res<-list(test=FALSE,mesage=paste("too many elements in ",deparse(substitute(list))," for type '",type,"'",sep=""))
-else{ 
-res<-list(test=TRUE,message="ok")
-i<-0
-repeat{ 
-i<-i+1
-if (sum(names(list)[i]==elements)==0){ 
-res$test<-FALSE
-break 
-}
-else {
-if (i==length(list)) break
-}
-}
-if (res$test==FALSE) {
-texte<-switch(i,'1'="st",'2'="nd",'3'="rd")
-if (is.null(texte)==TRUE) texte<-"th"
-res$message<-paste("invalid name '", names(list)[i],"' for ",i,texte," element of '",deparse(substitute(list)),"'",sep="")
-} 
-else{ 
-intNames<-c("fixItems")
-seedNames<-c("seed")
-singleIntNames<-c("nrItems")
-numNames<-c("theta","halfRange","thr","alpha","D")
-metNames<-c("method")
-priorNames<-c("priorDist")
-parNames<-c("priorPar","range")
-ruleNames<-c("rule")
-eapNames<-c("parInt")
-itemSelectNames<-c("itemSelect")
-infoTypeNames<-c("infoType")
-startNames<-c("startSelect")
-i<-0
-repeat{
-i<-i+1
-vect<-c(sum(names(list)[i]==intNames),
-sum(names(list)[i]==seedNames),
-sum(names(list)[i]==singleIntNames),
-sum(names(list)[i]==numNames),
-sum(names(list)[i]==metNames),
-sum(names(list)[i]==priorNames),
-sum(names(list)[i]==parNames),
-sum(names(list)[i]==ruleNames),
-sum(names(list)[i]==eapNames),
-sum(names(list)[i]==itemSelectNames),
-sum(names(list)[i]==infoTypeNames),
-sum(names(list)[i]==startNames))
-ind<-(1:12)[vect==1]
-prov<-switch(ind,
-'1'=ifelse(is.null(list[[i]]),TRUE,ifelse(is.numeric(list[[i]]),ifelse(max(abs(list[[i]]-round(list[[i]])))<=0.0001,TRUE,FALSE),FALSE)),
-'2'=ifelse(is.null(list[[i]]),TRUE,ifelse(is.numeric(list[[i]]),ifelse(length(list[[i]])==1,TRUE,FALSE),FALSE)),
-'3'=ifelse(is.numeric(list[[i]]) & length(list[[i]])==1,ifelse(abs(list[[i]]-round(list[[i]]))<=0.0001,TRUE,FALSE),FALSE),
-'4'=(is.numeric(list[[i]]) & length(list[[i]])==1),
-'5'=(is.list(list[[i]])==FALSE & length(list[[i]])==1 & sum(list[[i]]==c("ML","BM","WL","EAP"))==1),
-'6'=(is.list(list[[i]])==FALSE &length(list[[i]])==1 & sum(list[[i]]==c("norm","unif","Jeffreys"))==1),
-'7'=(is.numeric(list[[i]]) & length(list[[i]])==2),
-'8'=(is.list(list[[i]])==FALSE & length(list[[i]])==1 & sum(list[[i]]==c("length","precision","classification"))==1),
-'9'=(is.list(list[[i]])==FALSE & length(list[[i]])==3 & is.numeric(list[[i]])==TRUE & abs(list[[i]][3]-round(list[[i]][3]))<=0.0001),
-'10'=(is.list(list[[i]])==FALSE & length(list[[i]])==1 & sum(list[[i]]==c("MFI","Urry","MLWI","MPWI","MEI","MEPV","random"))==1),
-'11'=(is.list(list[[i]])==FALSE & length(list[[i]])==1 & sum(list[[i]]==c("observed","Fisher"))==1),
-'12'=(is.list(list[[i]])==FALSE & length(list[[i]])==1 & sum(list[[i]]==c("bOpt","MFI"))==1),
-)
-if (prov==FALSE){
-res$test<-FALSE
-res$message<-switch(ind,
-'1'=paste("element '",names(list)[i],"' of '",deparse(substitute(list)),"' must be a vector of integer values or NULL",sep=""),
-'2'=paste("element '",names(list)[i],"' of '",deparse(substitute(list)),"' must be a single numeric value or NULL",sep=""),
-'3'=paste("element '",names(list)[i],"' of '",deparse(substitute(list)),"' must be a single integer value",sep=""),
-'4'=paste("element '",names(list)[i],"' of '",deparse(substitute(list)),"' must be a single numeric value",sep=""),
-'5'=paste("element '",names(list)[i],"' of '",deparse(substitute(list)),"' must be either 'ML', 'BM', 'EAP' or 'WL'",sep=""),
-'6'=paste("element '",names(list)[i],"' of '",deparse(substitute(list)),"' must be either 'norm', 'unif'or 'Jeffreys'",sep=""),
-'7'=paste("element '",names(list)[i],"' of '",deparse(substitute(list)),"' must be a vector of two numeric values",sep=""),
-'8'=paste("element '",names(list)[i],"' of '",deparse(substitute(list)),"' must be either 'length', 'precision'","\n"," or 'classification'",sep=""),
-'9'=paste("element '",names(list)[i],"' of '",deparse(substitute(list)),"' must be a vector of two numeric and","\n"," one integer components",sep=""), 
-'10'=paste("element '",names(list)[i],"' of '",deparse(substitute(list)),"' must be either 'MFI', 'Urry',","\n"," 'MLWI', 'MPWI', 'MEI', 'MEPV' or 'random'",sep=""),
-'11'=paste("element '",names(list)[i],"' of '",deparse(substitute(list)),"' must be either 'observed' or 'Fisher'",sep=""),
-'12'=paste("element '",names(list)[i],"' of '",deparse(substitute(list)),"' must be either 'bOpt' or 'MFI'",sep="")
-)
-break
-}
-else {
-if (i==length(list)) break
-}
-}
-} 
-} 
-} 
-} 
-} 
-return(res)
+testList<-function (list, type = "start") 
+{
+    argNames = ls()
+    if (!is.list(list)) 
+        res <- list(test = FALSE, message = paste(deparse(substitute(list)), 
+            " is not a list", sep = ""))
+    else {
+        if (is.null(names(list))) 
+            res <- list(test = FALSE, message = paste("list '", 
+                deparse(substitute(list)), "' has no argument names", 
+                sep = ""))
+        else {
+            elements <- switch(type, start = c("fixItems", "seed", 
+                "nrItems", "theta", "halfRange", "startSelect"), 
+                test = c("method", "priorDist", "priorPar", "range", 
+                  "D", "parInt", "itemSelect", "infoType","randomesque"), stop = c("rule", 
+                  "thr", "alpha"), final = c("method", "priorDist", 
+                  "priorPar", "range", "D", "alpha", "parInt"))
+            if (is.null(elements)) 
+                res <- list(test = FALSE, message = paste("invalid 'type' argument ('", 
+                  type, "' is not allowed)", sep = ""))
+            else {
+                if (length(list) > length(elements)) 
+                  res <- list(test = FALSE, mesage = paste("too many elements in ", 
+                    deparse(substitute(list)), " for type '", 
+                    type, "'", sep = ""))
+                else {
+                  res <- list(test = TRUE, message = "ok")
+                  i <- 0
+                  repeat {
+                    i <- i + 1
+                    if (sum(names(list)[i] == elements) == 0) {
+                      res$test <- FALSE
+                      break
+                    }
+                    else {
+                      if (i == length(list)) 
+                        break
+                    }
+                  }
+                  if (!res$test) {
+                    texte <- switch(i, `1` = "st", `2` = "nd", 
+                      `3` = "rd")
+                    if (is.null(texte)) 
+                      texte <- "th"
+                    res$message <- paste("invalid name '", names(list)[i], 
+                      "' for ", i, texte, " element of '", deparse(substitute(list)), 
+                      "'", sep = "")
+                  }
+                  else {
+                    intNames <- c("fixItems")
+                    seedNames <- c("seed")
+                    singleIntNames <- c("nrItems")
+                    numNames <- c("theta", "halfRange", "thr", 
+                      "alpha", "D")
+                    metNames <- c("method")
+                    priorNames <- c("priorDist")
+                    parNames <- c("priorPar", "range")
+                    ruleNames <- c("rule")
+                    eapNames <- c("parInt")
+                    itemSelectNames <- c("itemSelect")
+                    infoTypeNames <- c("infoType")
+                    startNames <- c("startSelect")
+                    intOnlyNames <- c("randomesque")
+                    i <- 0
+                    repeat {
+                      i <- i + 1
+                      vect <- c(sum(names(list)[i] == intNames),  
+                        sum(names(list)[i] == seedNames), sum(names(list)[i] == 
+                          singleIntNames), sum(names(list)[i] == 
+                          numNames), sum(names(list)[i] == metNames), 
+                        sum(names(list)[i] == priorNames), sum(names(list)[i] == 
+                          parNames), sum(names(list)[i] == ruleNames), 
+                        sum(names(list)[i] == eapNames), sum(names(list)[i] == 
+                          itemSelectNames), sum(names(list)[i] == 
+                          infoTypeNames), sum(names(list)[i] == 
+                          startNames),sum(names(list)[i] == intOnlyNames))
+                      ind <- (1:13)[vect == 1]
+                      prov <- switch(ind, `1` = ifelse(is.null(list[[i]]), 
+                        TRUE, ifelse(is.numeric(list[[i]]), ifelse(max(abs(list[[i]] - 
+                          round(list[[i]]))) <= 1e-04, TRUE, 
+                          FALSE), FALSE)), `2` = ifelse(is.null(list[[i]]), 
+                        TRUE, ifelse(is.numeric(list[[i]]), ifelse(length(list[[i]]) == 
+                          1, TRUE, FALSE), FALSE)), `3` = ifelse(is.numeric(list[[i]]) & 
+                        length(list[[i]]) == 1, ifelse(abs(list[[i]] - 
+                        round(list[[i]])) <= 1e-04, TRUE, FALSE), 
+                        FALSE), `4` = (is.numeric(list[[i]]) & 
+                        length(list[[i]]) == 1), `5` = (is.list(list[[i]]) == 
+                        FALSE & length(list[[i]]) == 1 & sum(list[[i]] == 
+                        c("ML", "BM", "WL", "EAP")) == 1), `6` = (is.list(list[[i]]) == 
+                        FALSE & length(list[[i]]) == 1 & sum(list[[i]] == 
+                        c("norm", "unif", "Jeffreys")) == 1), 
+                        `7` = (is.numeric(list[[i]]) & length(list[[i]]) == 
+                          2), `8` = (is.list(list[[i]]) == FALSE & 
+                          length(list[[i]]) == 1 & sum(list[[i]] == 
+                          c("length", "precision", "classification")) == 
+                          1), `9` = (is.list(list[[i]]) == FALSE & 
+                          length(list[[i]]) == 3 & is.numeric(list[[i]]) == 
+                          TRUE & abs(list[[i]][3] - round(list[[i]][3])) <= 
+                          1e-04), `10` = (is.list(list[[i]]) == 
+                          FALSE & length(list[[i]]) == 1 & sum(list[[i]] == 
+                          c("MFI", "Urry", "MLWI", "MPWI", "MEI", 
+                            "MEPV", "random")) == 1), `11` = (is.list(list[[i]]) == 
+                          FALSE & length(list[[i]]) == 1 & sum(list[[i]] == 
+                          c("observed", "Fisher")) == 1), `12` = (is.list(list[[i]]) == 
+                          FALSE & length(list[[i]]) == 1 & sum(list[[i]] == 
+                          c("bOpt", "MFI")) == 1), `13` = ifelse(is.numeric(list[[i]]), ifelse(max(abs(list[[i]] - 
+                          round(list[[i]]))) <= 1e-04, ifelse(list[[i]]>0,TRUE,FALSE), 
+                          FALSE), FALSE))
+                      if (!prov) {
+                        res$test <- FALSE
+                        res$message <- switch(ind, `1` = paste("element '", 
+                          names(list)[i], "' of '", deparse(substitute(list)), 
+                          "' must be a vector of integer values or NULL", 
+                          sep = ""), `2` = paste("element '", 
+                          names(list)[i], "' of '", deparse(substitute(list)), 
+                          "' must be a single numeric value or NULL", 
+                          sep = ""), `3` = paste("element '", 
+                          names(list)[i], "' of '", deparse(substitute(list)), 
+                          "' must be a single integer value", 
+                          sep = ""), `4` = paste("element '", 
+                          names(list)[i], "' of '", deparse(substitute(list)), 
+                          "' must be a single numeric value", 
+                          sep = ""), `5` = paste("element '", 
+                          names(list)[i], "' of '", deparse(substitute(list)), 
+                          "' must be either 'ML', 'BM', 'EAP' or 'WL'", 
+                          sep = ""), `6` = paste("element '", 
+                          names(list)[i], "' of '", deparse(substitute(list)), 
+                          "' must be either 'norm', 'unif'or 'Jeffreys'", 
+                          sep = ""), `7` = paste("element '", 
+                          names(list)[i], "' of '", deparse(substitute(list)), 
+                          "' must be a vector of two numeric values", 
+                          sep = ""), `8` = paste("element '", 
+                          names(list)[i], "' of '", deparse(substitute(list)), 
+                          "' must be either 'length', 'precision'", 
+                          "\n", " or 'classification'", sep = ""), 
+                          `9` = paste("element '", names(list)[i], 
+                            "' of '", deparse(substitute(list)), 
+                            "' must be a vector of two numeric and", 
+                            "\n", " one integer components", 
+                            sep = ""), `10` = paste("element '", 
+                            names(list)[i], "' of '", deparse(substitute(list)), 
+                            "' must be either 'MFI', 'Urry',", 
+                            "\n", " 'MLWI', 'MPWI', 'MEI', 'MEPV' or 'random'", 
+                            sep = ""), `11` = paste("element '", 
+                            names(list)[i], "' of '", deparse(substitute(list)), 
+                            "' must be either 'observed' or 'Fisher'", 
+                            sep = ""), `12` = paste("element '", 
+                            names(list)[i], "' of '", deparse(substitute(list)), 
+                            "' must be either 'bOpt' or 'MFI'", 
+                            sep = ""),`13` = paste("element '", 
+                          names(list)[i], "' of '", deparse(substitute(list)), 
+                          "' must be a positive integer value",sep=""))
+                        break
+                      }
+                      else {
+                        if (i == length(list)) 
+                          break
+                      }
+                    }
+                  }
+                }
+            }
+        }
+    }
+    return(res)
 }
